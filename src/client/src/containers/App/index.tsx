2a9cab29a9d8cee49bb8c56378d38a885e4a5d4e
olha-yakymovych <msolya@outlook.com>
2021-02-20 17:21:37 +0200

    added base models and redux store

--- src/client/src/containers/App/index.tsx
+++ src/client/src/containers/App/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import Routing from '../Routing';
 
 import { history } from '../../helpers/history.helper';
+import configureStore from '../../store';
+
+const store = configureStore();
 
 const App: React.FC = (): React.ReactElement => {
   return (
-    <Router history={history}>
-      <Routing />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routing />
+      </Router>
+    </Provider>
   );
 };
 
