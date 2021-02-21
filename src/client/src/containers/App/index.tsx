076914602b08f1587abc9ad20286ed85a3466c2c
olha-yakymovych <msolya@outlook.com>
2021-02-21 02:41:19 +0200

    fixed routing navigation

--- src/client/src/containers/App/index.tsx
+++ src/client/src/containers/App/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Grommet } from 'grommet';
 
 import Routing from '../Routing';
